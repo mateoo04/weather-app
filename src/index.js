0741832f5fbbb6f49daf28426f11416174efbaa9
mateoo04 <144594500+mateoo04@users.noreply.github.com>
2024-08-11 22:06:48 +0200

    Fetch data and display
    10 day weather forecast is fetched by Visual Crossing API and displayed.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-import "./style.css";
+import './style.css';
+
+const getData = async function getWeatherData() {
+  const response = await fetch(
+    'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Mostar/next10days?key=JV3JYYEPPXM5J6NXHJZ5UF3FY&unitGroup=metric&include=days&elements=datetime,icon,tempmax,tempmin,precipprob,uvindex',
+    { mode: 'cors' }
+  );
+
+  const parsedResponse = await response.json();
+
+  return parsedResponse.days;
+};
+
+function displayWeather() {
+  const tenDayWeather = document.querySelector('.ten-day-weather');
+
+  getData().then((days) => {
+    days.forEach((item) => {
+      const day = document.createElement('div');
+
+      const icon = document.createElement('p');
+      icon.textContent = item.icon;
+
+      const tempContainer = document.createElement('div');
+      tempContainer.classList.add('temp-container');
+
+      const maxTemp = document.createElement('p');
+      maxTemp.textContent = item.tempmax;
+      const minTemp = document.createElement('p');
+      minTemp.textContent = item.tempmin;
+      tempContainer.append(maxTemp, minTemp);
+
+      day.append(icon, tempContainer);
+
+      tenDayWeather.append(day);
+    });
+  });
+}
+
+displayWeather();
