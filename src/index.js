817e1ebea754126535dc924ec3045132d9c07eea
mateoo04 <144594500+mateoo04@users.noreply.github.com>
2024-08-12 10:40:49 +0200

    Add weather icons
    Load and display additional data, add input bar for location

--- src/index.js
+++ src/index.js
@@ -1,39 +1,104 @@
 import './style.css';
+import clearDay from './clear-day.svg';
+import partlyCloudyDay from './partly-cloudy-day.svg';
+import partlyCloudyNight from './partly-cloudy-night.svg';
+import clearNight from './clear-night.svg';
+import rain from './rain.svg';
+import snow from './snow.svg';
+import fog from './fog.svg';
+import wind from './wind.svg';
+import cloudy from './cloudy.svg';
+
+let location = 'Zagreb';
+let unit = 'metric';
+
+const getIcon = function getCorrespondingWeatherIcon(weather) {
+  switch (weather) {
+    case 'clear-day':
+      return clearDay;
+    case 'partly-cloudy-day':
+      return partlyCloudyDay;
+    case 'partly-cloudy-night':
+      return partlyCloudyNight;
+    case 'clear-night':
+      return clearNight;
+    case 'rain':
+      return rain;
+    case 'snow':
+      return snow;
+    case 'fog':
+      return fog;
+    case 'wind':
+      return wind;
+    case 'cloudy':
+      return cloudy;
+    default:
+      return null;
+  }
+};
 
 const getData = async function getWeatherData() {
   const response = await fetch(
-    'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Mostar/next10days?key=JV3JYYEPPXM5J6NXHJZ5UF3FY&unitGroup=metric&include=days&elements=datetime,icon,tempmax,tempmin,precipprob,uvindex',
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next10days?key=JV3JYYEPPXM5J6NXHJZ5UF3FY&unitGroup=${unit}&include=current%2Cdays&elements=datetime,icon,temp,tempmax,tempmin,precipprob,uvindex,feelslike,humidity,windspeed,conditions`,
     { mode: 'cors' }
   );
 
   const parsedResponse = await response.json();
 
-  return parsedResponse.days;
+  console.log(parsedResponse);
+
+  return {
+    days: parsedResponse.days,
+    current: parsedResponse.currentConditions,
+  };
 };
 
 function displayWeather() {
-  const tenDayWeather = document.querySelector('.ten-day-weather');
+  const tenDayWeatherContainer = document.querySelector('.ten-day-weather');
 
-  getData().then((days) => {
-    days.forEach((item) => {
+  //10 day weather
+  getData().then((data) => {
+    data.days.forEach((item) => {
       const day = document.createElement('div');
+      day.classList.add('day-item');
 
-      const icon = document.createElement('p');
-      icon.textContent = item.icon;
+      const icon = document.createElement('img');
+      icon.src = getIcon(item.icon);
 
-      const tempContainer = document.createElement('div');
-      tempContainer.classList.add('temp-container');
+      const date = document.createElement('p');
+      date.textContent = item.datetime;
 
-      const maxTemp = document.createElement('p');
-      maxTemp.textContent = item.tempmax;
-      const minTemp = document.createElement('p');
-      minTemp.textContent = item.tempmin;
-      tempContainer.append(maxTemp, minTemp);
+      const temperature = document.createElement('p');
+      temperature.classList.add('temperature');
+      temperature.textContent = item.tempmax + '°/';
+      const minTemp = document.createElement('span');
+      minTemp.classList.add('min-temp');
+      minTemp.textContent = item.tempmin + '°';
+      temperature.append(minTemp);
 
-      day.append(icon, tempContainer);
+      day.append(icon, date, temperature);
 
-      tenDayWeather.append(day);
+      tenDayWeatherContainer.append(day);
     });
+
+    //current weather
+    const currentWeatherIcon = document.querySelector('.current-weather-icon');
+    const currentTemp = document.querySelector('.current-temp');
+    const currentConditions = document.querySelector('.conditions');
+    const currentFeelsLike = document.querySelector('.feels-like');
+    const currentUv = document.querySelector('.uv-index .value');
+    const currentHumdity = document.querySelector('.humidity .value');
+    const currentWind = document.querySelector('.wind .value');
+    const currentPrecipProb = document.querySelector('.precip-prob .value');
+
+    currentWeatherIcon.src = getIcon(data.current.icon);
+    currentTemp.textContent = data.current.temp + '°';
+    currentConditions.textContent = data.current.conditions;
+    currentFeelsLike.textContent = data.current.feelslike;
+    currentUv.textContent = data.current.uvindex;
+    currentHumdity.textContent = data.current.humidity;
+    currentWind.textContent = data.current.windspeed;
+    currentPrecipProb.textContent = data.current.precipprob;
   });
 }
 
